bdf1b82eb7f3425e628f942fa30487238deb6244
Orcun Guler <orcunguler381@gmail.com>
2022-10-05 01:13:03 +0300

    add sass and main logic of hook

--- src/hooks/useMultistepForm.ts
+++ src/hooks/useMultistepForm.ts
@@ -1,7 +1,27 @@
 import { ReactElement, useState } from 'react'
 
-const useMultiStepform = (stpes: ReactElement[]) => {
+const useMultiStepForm = (steps: ReactElement[]) => {
   const [currentStep, setCurrentStep] = useState(0)
+
+  const next = () => {
+    setCurrentStep(prev => (prev >= steps.length ? prev : prev + 1))
+  }
+
+  const prev = () => {
+    setCurrentStep(prev => (prev <= 0 ? prev : prev - 1))
+  }
+
+  const goTo = (index: number) => {
+    setCurrentStep(index)
+  }
+
+  return {
+    currentStep,
+    step: steps[currentStep],
+    next,
+    prev,
+    goTo,
+  }
 }
 
-export default useMultiStepform
+export default useMultiStepForm
